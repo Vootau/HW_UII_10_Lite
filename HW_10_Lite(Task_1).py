8a8f48a45944a5b80a94ec50788fa6c995836bba
Виктор Удалов <vootau@yandex.ru>
2025-01-19 19:21:11 +0300

    HW_UII_10_Lite(done). First try.

--- HW_10_Lite(Task_1).py
+++ HW_10_Lite(Task_1).py
@@ -1,22 +1,22 @@
 
-def convert_to_int(x):
+def convert_to_int(x: int):
     try:
         result = int(x)
     except ValueError:
-        print(f'Ошибка: "{x}" должно быть числом, а это {type(x)}')
+        print(f'Ошибка: "{x}" должно быть числом, а это {type(x).__name__}.')
     except TypeError:
-        print(f'Ошибка "{x}" должно быть числом, а это {type(x)}')
+        print(f'Ошибка "{x}" должно быть числом, а это {type(x).__name__}.')
     except Exception as e:
         print(f'Упс! Ошибка: {e}')
     else:
-        print(f'Успешное преобразование: {result} теперь {type(result)}')
+        print(f'Успешное преобразование: {result} теперь {type(result).__name__}.')
     finally:
-        print('\tПреобразование выполнено')
+        print('\tПреобразование выполнено.')
 
 
 convert_to_int(123)
 
-convert_to_int([1,2,3])
+convert_to_int([1, 2, 3])
 
 convert_to_int("abc")
 
--- HW_10_Lite(Task_2).py
+++ HW_10_Lite(Task_2).py
@@ -1,34 +1,32 @@
 def validate_user_input(value):
     # Проверка, что data является словарем
     if not isinstance(value, dict):
-        raise TypeError("Data must be a dictionary")
+        raise TypeError("Данные должны быть словарем.")
 
     try:
         # Проверка типа значения (строка) для ключа 'name'
         if not isinstance(value['name'], str):
-            raise ValueError("Name must be a string")
+            raise ValueError("Ключ 'name' должен быть строкой.")
 
         # Проверка типа значения (число) для ключа 'age'
         if not isinstance(value['age'], int):
-            raise TypeError(f"Age must be a positive integer not a {type(value['age']).__name__}")
+            raise TypeError(f"Значение ключа 'age' должно быть числом, а не {type(value['age']).__name__}.")
 
         # Проверка ключа 'age' (положительное число)
         elif int(value['age']) <= 0:
-            raise ValueError("Age must be a positive integer")
-
-
+            raise ValueError("Возраст не может быть меньше 0.")
 
     except KeyError as ke:
-        raise ValueError(f"Missing required key: {ke}")
+        raise ValueError(f"Отсутсвует нужный ключ: {ke}")
 
 # Корректный словарь
 data = {"name": "Alice", "age": 30}
 
 try:
     validate_user_input(data)
-    print("Validation successful!")
+    print("Валидация успешна!")
 except Exception as e:
-    print(f"Validation failed: {e}")
+    print(f"Валидация провалена: {e}")
 
 # Словарь без ключа 'name'
 # data = {"age": 30}
@@ -38,33 +36,33 @@ data = {'name': 30, "age": 30}
 
 try:
     validate_user_input(data)
-    print("Validation successful!")
+    print("Валидация успешна!")
 except Exception as e:
-    print(f"Validation failed: {e}")
+    print(f"Валидация провалена: {e}")
 
 # Словарь со строковым значением для 'age'
 data = {"name": "Bob", "age": "thirty"}
 
 try:
     validate_user_input(data)
-    print("Validation successful!")
+    print("Валидация успешна!")
 except Exception as e:
-    print(f"Validation failed: {e}")
+    print(f"Валидация провалена: {e}")
 
 # Словарь с отрицательным значением для 'age'
 data = {"name": "Charlie", "age": -10}
 
 try:
     validate_user_input(data)
-    print("Validation successful!")
+    print("Валидация успешна!")
 except Exception as e:
-    print(f"Validation failed: {e}")
+    print(f"Валидация провалена: {e}")
 
 # Строка вместо словаря
-data = "incorrect_data_type"
+data = "какие-то данные"
 
 try:
     validate_user_input(data)
-    print("Validation successful!")
+    print("Валидация успешна!")
 except Exception as e:
-    print(f"Validation failed: {e}")
+    print(f"Валидация провалена: {e}")
